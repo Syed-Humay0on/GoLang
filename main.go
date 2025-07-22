82d3d8f071f802ea8821f41bd5d0a121ba063fba
Proto <Humayoon04syed@gmail.com>
2025-07-22 21:12:20 +0500

    func && switch case

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	_ "os" // add a underscore prefix to get rid of compile time error for unused packages
+)
 
 func main() {
-	// a curly bracket  { cannot appear in the begining of a line in go
-	// comments start with double slash or
+	// a curly bracket { cannot appear in the begining of a line in go
+	// comments start with double slash or slash aestrik for multi line code
 	/*
 		multi
 		line
 		comment
 	*/
-	var intNum int
-	fmt.Println(intNum)
+	myName := "Emma Watson" //decalre a variable using := (only inside a function)
+	myName = "Zain Malik"   // reassign that declared variable
+
+	//switch case
+	switch myName {
+
+	case ("Emma Watson"):
+		fmt.Println("Queen")
+
+	case ("Zain Malik"):
+		fmt.Println("Chad")
+
+	default:
+		fmt.Println("Absolutely Nobody")
+	}
 }
