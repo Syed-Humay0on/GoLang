ad85733c75e6dda89f7871129139171298e8fc83
Proto <Humayoon04syed@gmail.com>
2025-07-22 18:37:32 +0500

    basics

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello! world")
+	// a curly bracket  { cannot appear in the begining of a line in go
+	// comments start with double slash or
+	/*
+		multi
+		line
+		comment
+	*/
+	var intNum int
+	fmt.Println(intNum)
 }
