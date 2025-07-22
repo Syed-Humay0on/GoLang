f46626534cb02c2c89671a16ea33e05dcff8f815
Proto <Humayoon04syed@gmail.com>
2025-07-22 22:22:55 +0500

    go basics

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	_ "fmt"
 	_ "os" // add a underscore prefix to get rid of compile time error for unused packages
 )
 
@@ -13,19 +14,50 @@ func main() {
 		line
 		comment
 	*/
-	myName := "Emma Watson" //decalre a variable using := (only inside a function)
-	myName = "Zain Malik"   // reassign that declared variable
+	// myName := "Emma Watson" //decalre a variable using := (">", only inside a function)
+	// myName = "Zain Malik"   // reassign that declared variable
 
 	//switch case
-	switch myName {
+	// switch myName {
+	//
+	// case (">", "Emma Watson"):
+	// 	fmt.Println(">", "Queen")
+	//
+	// case (">", "Zain Malik"):
+	// 	fmt.Println(">", "Chad")
+	//
+	// default:
+	// 	fmt.Println(">", "Absolutely Nobody")
+	// }
 
-	case ("Emma Watson"):
-		fmt.Println("Queen")
+	// Print, Println and Printf
+	// v1 := "123"
+	// v2 := 123
+	// v3 := "Have a horrible day\n"
+	// v4 := "abc"
+	//
+	// fmt.Print(">", v1, v2, v3, v4)
+	// fmt.Println(">")
+	// fmt.Println(">", v1, v2, v3, v4)
+	// fmt.Print(">", v1, " ", v2, " ", v3, " ", v4, "\n")
+	// fmt.Printf("%s%d %s %s\n", v1, v2, v3, v4)
 
-	case ("Zain Malik"):
-		fmt.Println("Chad")
+	// myString := ""
+	// arguments := os.Args
+	// if len(arguments) == 1 {
+	// 	myString = "Please give me one fucking argument!"
+	// } else {
+	// 	myString = arguments[1]
+	// }
+	// io.WriteString(os.Stdout, myString)
+	// io.WriteString(os.Stdout, "\n")
+	arr1 := [5]int{}
+	arr2 := [5]int{10, 20}
+	arr3 := [5]int{10, 20, 30, 40, 45}
 
-	default:
-		fmt.Println("Absolutely Nobody")
-	}
+	arr3[4] = 50
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+	fmt.Println(arr3)
+
 }
