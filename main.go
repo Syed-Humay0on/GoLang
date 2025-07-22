65cef65c6f26c69cc75cb2c6afe228c73f577155
Proto <Humayoon04syed@gmail.com>
2025-07-22 22:27:37 +0500

    go basics

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	_ "fmt"
-	_ "os" // add a underscore prefix to get rid of compile time error for unused packages
+	"io"
+	"os"
+	// add a underscore prefix to get rid of compile time error for unused packages
 )
 
 func main() {
@@ -14,43 +15,47 @@ func main() {
 		line
 		comment
 	*/
-	// myName := "Emma Watson" //decalre a variable using := (">", only inside a function)
-	// myName = "Zain Malik"   // reassign that declared variable
 
 	//switch case
-	// switch myName {
-	//
-	// case (">", "Emma Watson"):
-	// 	fmt.Println(">", "Queen")
-	//
-	// case (">", "Zain Malik"):
-	// 	fmt.Println(">", "Chad")
-	//
-	// default:
-	// 	fmt.Println(">", "Absolutely Nobody")
-	// }
+	myName := "Emma Watson" //decalre a variable using := (">", only inside a function)
+	myName = "Zain Malik"   // reassign that declared variable
 
-	// Print, Println and Printf
-	// v1 := "123"
-	// v2 := 123
-	// v3 := "Have a horrible day\n"
-	// v4 := "abc"
-	//
-	// fmt.Print(">", v1, v2, v3, v4)
-	// fmt.Println(">")
-	// fmt.Println(">", v1, v2, v3, v4)
-	// fmt.Print(">", v1, " ", v2, " ", v3, " ", v4, "\n")
-	// fmt.Printf("%s%d %s %s\n", v1, v2, v3, v4)
+	switch myName {
 
-	// myString := ""
-	// arguments := os.Args
-	// if len(arguments) == 1 {
-	// 	myString = "Please give me one fucking argument!"
-	// } else {
-	// 	myString = arguments[1]
-	// }
-	// io.WriteString(os.Stdout, myString)
-	// io.WriteString(os.Stdout, "\n")
+	case ("Emma Watson"):
+		fmt.Println(">", "Queen")
+
+	case ("Zain Malik"):
+		fmt.Println("Chad")
+
+	default:
+		fmt.Println("Absolutely Nobody")
+	}
+
+	//	Print, Println and Printf
+	v1 := "123"
+	v2 := 123
+	v3 := "Have a horrible day\n"
+	v4 := "abc"
+
+	fmt.Print(">", v1, v2, v3, v4)
+	fmt.Println(">")
+	fmt.Println(">", v1, v2, v3, v4)
+	fmt.Print(">", v1, " ", v2, " ", v3, " ", v4, "\n")
+	fmt.Printf("%s%d %s %s\n", v1, v2, v3, v4)
+
+	//standard output
+	myString := ""
+	arguments := os.Args
+	if len(arguments) == 1 {
+		myString = "Please give me one fucking argument!"
+	} else {
+		myString = arguments[1]
+	}
+	io.WriteString(os.Stdout, myString)
+	io.WriteString(os.Stdout, "\n")
+
+	//Go Arrays
 	arr1 := [5]int{}
 	arr2 := [5]int{10, 20}
 	arr3 := [5]int{10, 20, 30, 40, 45}
