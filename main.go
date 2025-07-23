080cdd9dec757d7c9720a6e087714cca46c81321
Proto <Humayoon04syed@gmail.com>
2025-07-23 17:30:38 +0500

    slices and maps

--- main.go
+++ main.go
@@ -1,68 +1,59 @@
 package main
 
 import (
+	_ "errors"
 	"fmt"
-	"io"
-	"os"
-	// add a underscore prefix to get rid of compile time error for unused packages
+	_ "unicode/utf8"
 )
 
 func main() {
-	// a curly bracket { cannot appear in the begining of a line in go
-	// comments start with double slash or slash aestrik for multi line code
-	/*
-		multi
-		line
-		comment
-	*/
+	// var intNum rune = 'a'
+	// 	var numerator int = 88
+	// 	var denominator int = 0
+	// 	var result, remainder, err = intDivision(numerator, denominator)
+	// 	// if err != nil {
+	// 	// 	fmt.Printf(err.Error())
+	// 	// } else if remainder == 0 {
+	// 	// 	fmt.Printf("The result of the division is %v", result)
+	// 	// } else {
+	// 	// 	fmt.Printf("The result of the division is %v with remainder %v", result, remainder)
+	// 	// }
+	//
+	// 	//switch statement
+	// 	switch {
+	// 	case err != nil:
+	// 		fmt.Printf(err.Error())
+	// 	case remainder == 0:
+	// 		fmt.Printf("The Result of the int division is %v", result)
+	// 	default:
+	// 		fmt.Printf("The result of the int division is %v with remainder: %v", result, remainder)
+	// 	}
+	// }
+	// func intDivision(numerator int, denominator int) (int, int, error) {
+	// 	var err error
+	// 	if denominator == 0 {
+	// 		err = errors.New("Cannot divide by zero")
+	// 		return 0, 0, err
+	// 	}
+	// 	var result int = numerator / denominator
+	// 	var remainder int = numerator % denominator
+	// 	return result, remainder, err
 
-	//switch case
-	myName := "Emma Watson" //decalre a variable using := (">", only inside a function)
-	myName = "Zain Malik"   // reassign that declared variable
+	//SLICES
+	slice_Name := []string{"Humayoon", "Rehman", "Mujtaba", "Talha"}
+	length, capacity := len(slice_Name), cap(slice_Name)
+	fmt.Printf("The length of the slice is: %v and the capacity of the slice is: %v", length, capacity)
 
-	switch myName {
-
-	case ("Emma Watson"):
-		fmt.Println(">", "Queen")
-
-	case ("Zain Malik"):
-		fmt.Println("Chad")
-
-	default:
-		fmt.Println("Absolutely Nobody")
-	}
-
-	//	Print, Println and Printf
-	v1 := "123"
-	v2 := 123
-	v3 := "Have a horrible day\n"
-	v4 := "abc"
-
-	fmt.Print(">", v1, v2, v3, v4)
-	fmt.Println(">")
-	fmt.Println(">", v1, v2, v3, v4)
-	fmt.Print(">", v1, " ", v2, " ", v3, " ", v4, "\n")
-	fmt.Printf("%s%d %s %s\n", v1, v2, v3, v4)
-
-	//standard output
-	myString := ""
-	arguments := os.Args
-	if len(arguments) == 1 {
-		myString = "Please give me one fucking argument!"
-	} else {
-		myString = arguments[1]
-	}
-	io.WriteString(os.Stdout, myString)
-	io.WriteString(os.Stdout, "\n")
-
-	//Go Arrays
-	arr1 := [5]int{}
-	arr2 := [5]int{10, 20}
-	arr3 := [5]int{10, 20, 30, 40, 45}
-
-	arr3[4] = 50
-	fmt.Println(arr1)
-	fmt.Println(arr2)
-	fmt.Println(arr3)
-
+	// var myMap = map[string]uint8{"adam": 22, "Humayoon": 23}
+	// fmt.Println(myMap["Humayoon"])
+	// var age, ok = myMap["Imran"]
+	// if ok {
+	// 	fmt.Printf("The age is %v", age)
+	// } else {
+	// 	fmt.Printf("Invalid Key")
+	// }
+	//ITERATE THROUGH MAPS OR SLICE OR EVEN ARRAYS
+	// for name := range myMap {
+	// 	fmt.Printf("Name: %v\n", name)
+	// }
 }
