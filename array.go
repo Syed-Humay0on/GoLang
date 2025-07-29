b8bb157e2702eb3a66d6f725c6035c6006755d02
Proto <Humayoon04syed@gmail.com>
2025-07-29 18:09:23 +0500

    arrays

--- array.go
+++ array.go
@@ -1,19 +1,5 @@
 package main
 
-import "fmt"
-
-func twoSum(nums []int, target int) []int {
-	seen := make(map[int]int) // value -> index
-
-	for i, n := range nums {
-		if j, ok := seen[target-n]; ok {
-			ans := []int{j, i}
-			fmt.Println("answer:", ans) // print here
-			fmt.Println("nums/target:", nums, target)
-			return ans
-		}
-		seen[n] = i
-	}
-	fmt.Println("NO Solution? nums/target:", nums, target)
-	return nil
+func GetNames() []string {
+	return []string{"Humayoon", "Elizabeth", "Stephanie"}
 }
--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	_ "errors"
 	"fmt"
-	"time"
 	_ "unicode/utf8"
 )
 
 func main() {
-	nums := []int{2, 7, 11, 15}
-	target := 9
-	go twoSum(nums, target)
-	time.Sleep(time.Second * 2)
-	fmt.Println("Hello Web")
+	// nums := []int{2, 7, 11, 15}
+	// target := 9
+	// go twoSum(nums, target)
+	// time.Sleep(time.Second * 2)
+	// fmt.Println("Hello Web")
+	names := GetNames()
+	fmt.Println(names)
 }
