238f70a514a7fe15000f2b4030b9ce0d88173dcf
Proto <Humayoon04syed@gmail.com>
2025-09-14 16:28:22 +0500

    errors handling

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-	_ "errors"
+	"errors"
 	"fmt"
-	_ "unicode/utf8"
 )
 
 func main() {
-	// nums := []int{2, 7, 11, 15}
-	// target := 9
-	// go twoSum(nums, target)
-	// time.Sleep(time.Second * 2)
-	// fmt.Println("Hello Web")
-	names := GetNames()
-	fmt.Println(names, len(names))
+	var name1 string = "Humayoon"
+	var age1 int = 16
+	var name, age, err = Details(name1, age1)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Welcome to HSX Mr. %v. You're %v years old\n", name, age)
+	}
+}
+
+func Details(Name1 string, age int) (string, int, error) {
+	var err error
+	if Name1 != "Humayoon" || age < 18 {
+		return "", 0, errors.New("Intruder")
+
+	}
+	return Name1, age, err
 }
