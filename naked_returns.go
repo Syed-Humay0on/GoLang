f24a26812596f9b3cbf596132c74660968eef9f9
Proto <Humayoon04syed@gmail.com>
2026-01-15 00:20:26 +0500

    naked returns

---
+++ naked_returns.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func yearsUntilEvents(age int) (yearsUntilAdult int, yearsUntilDrinking int, yearsUntilCarRental int) {
+	yearsUntilAdult = max(0, 18-age)
+
+	yearsUntilDrinking = max(0, 21-age)
+
+	yearsUntilCarRental = max(0, 25-age)
+
+	return
+}
+func main() {
+	adult, drinking, car := yearsUntilEvents(13)
+	fmt.Println(adult, drinking, car)
+
+}
