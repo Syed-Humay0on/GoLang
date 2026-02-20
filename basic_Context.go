45cafdea6b072cc815977c952b80abf6965f8fe0
Proto <Humayoon04syed@gmail.com>
2026-02-20 20:08:58 +0500

    🔹GoLang: [Context basics]

--- basic_Context.go
+++ basic_Context.go
@@ -5,9 +5,18 @@ import (
 	"fmt"
 )
 
-func main() {
-	ctx := context.Background() // yeh humara root context hai
+func encrichContext(ctx context.Context) context.Context {
+	return context.WithValue(ctx, "Request-id", "12345")
+}
 
-	fmt.Println("Context bana liya!")
-	// abhi kuch nahi karta, bas shuru karne ke liye
+func doSomethingCool(ctx context.Context) {
+	rID := ctx.Value("Request-id")
+	fmt.Println(rID)
+}
+
+func main() {
+	fmt.Println("Go Context basic understanding")
+	ctx := context.Background()
+	ctx = encrichContext(ctx)
+	doSomethingCool(ctx)
 }
