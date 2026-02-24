028fe278cf3c97817a58b249f299b35f2898735f
Proto <Humayoon04syed@gmail.com>
2026-02-24 20:05:03 +0500

    🔹GoLang: [Channels]

--- channelsB.go
+++ channelsB.go
@@ -2,26 +2,39 @@ package main
 
 import (
 	"fmt"
-	"time"
+	_ "time"
 )
 
 func main() {
-	// create an unbuffered channel
+	// create a channel
 	ch := make(chan int)
 
+	// start a goroutine to send data to the channel
 	go func() {
-		fmt.Println("Sending value 1 to channel")
+		// send data to the channel
 		ch <- 1
-		fmt.Println("After sending value 1")
 	}()
 
-	// sleep for 3 seconds to ensure the goroutine has time to send the value
-	time.Sleep(3 * time.Second)
-
-	fmt.Println("Receiving value from channel")
+	// receive data from the channel
 	val := <-ch
 	fmt.Println(val)
 
-	// sleep 1 second to ensure goroutine has time to finish goroutine
-	time.Sleep(1 * time.Second)
+	// // create an unbuffered channel
+	// ch := make(chan int)
+	//
+	// go func() {
+	// 	fmt.Println("Sending value 1 to channel")
+	// 	ch <- 1
+	// 	fmt.Println("After sending value 1")
+	// }()
+	//
+	// // sleep for 3 seconds to ensure the goroutine has time to send the value
+	// time.Sleep(3 * time.Second)
+	//
+	// fmt.Println("Receiving value from channel")
+	// val := <-ch
+	// fmt.Println(val)
+	//
+	// // sleep 1 second to ensure goroutine has time to finish goroutine
+	// time.Sleep(1 * time.Second)
 }
